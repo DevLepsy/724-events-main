c632afee303c6d15eed31a2a6bf48fa7b7317bc8
DevLepsy <henryck.paris@outlook.fr>
2024-04-10 15:51:25 +0200

    Refactor Slider component to fix sorting and index issues

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,60 +8,56 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    /* Inversement des valeurs dans la condition, initialement -1 : 1 pour obtenir l'odre décroissant */
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
 
-  const nextCard = () => {
-    setTimeout(() => {
-      setIndex((index + 1) % (byDateDesc?.length || 0));
-    }, 5000);
-  };
+  // Ajout d'une constante pour initialiser l'indice dans notre tableau pour la partie focus
+  const ImgFocus = data?.focus.length;
 
+  const nextCard = () => {
+    /* Ajout de -1 pour que la taille du tableau corresonde bien à l'indice actuel */
+    setTimeout(() => setIndex(index < ImgFocus - 1 ? index + 1 : 0), 5000);
+  };
   useEffect(() => {
     nextCard();
   });
-
-  function generateUniqueKey() {
-    return Math.random().toString(36).substring(2) + Date.now().toString(36);
-  }
-
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div key={`${event.date}-${generateUniqueKey()}`}>
-          <div
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={generateUniqueKey()}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  onChange={() => {}}
-                  // onClick={() => {
-                  //   setIndex(radioIdx);
-                  // }}
-                />
-              ))}
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
         </div>
       ))}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {/* Ajout de focus en paramètre pour créer des bullets en fonction de la clé focus dans notre tableau */}
+          {byDateDesc?.map((focus, radioIdx) => (
+            <input
+              key={`${focus.title}`}
+              type="radio"
+              name="radio-button"
+              /* Syntaxe modifiée idx en index */
+              checked={index === radioIdx}
+              /* Ajout de readOnly pour la gestion de l'état */
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
