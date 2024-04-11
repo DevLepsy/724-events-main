84737c4347b5e64c921a81801f15b1833375a2cd
DevLepsy <henryck.paris@outlook.fr>
2024-04-11 10:02:05 +0200

    Add spacebar key handling to pause and resume slider in Slider component tests

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Slider from "./index";
 import { api, DataProvider } from "../../contexts/DataContext";
 
@@ -26,9 +26,8 @@ const data = {
   ],
 };
 
-describe("When slider is created", () => {
-  it("a list card is displayed", async () => {
-    window.console.error = jest.fn();
+describe("Slider", () => {
+  it("displays a list card", async () => {
     api.loadData = jest.fn().mockReturnValue(data);
     render(
       <DataProvider>
@@ -40,5 +39,39 @@ describe("When slider is created", () => {
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
+  });
+
+  it("pauses and resumes slider when spacebar is pressed", async () => {
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    // Vérifiez que le slider est en cours de lecture
+    const initialTitle = await screen.findByText("World economic forum");
+    const initialDescription = await screen.findByText(
+      "Oeuvre à la coopération entre le secteur public et le privé."
+    );
+
+    // Simuler l'appui sur la barre d'espace pour mettre en pause le slider
+    fireEvent.keyDown(window, { key: " ", code: "Space" });
+
+    // Vérifiez si le slider est en pause
+    const pausedTitle = await screen.findByText("World economic forum");
+    const pausedDescription = await screen.findByText(
+      "Oeuvre à la coopération entre le secteur public et le privé."
+    );
+
+    expect(pausedTitle).toEqual(initialTitle);
+    expect(pausedDescription).toEqual(initialDescription);
+
+    // Simuler l'appui sur la barre d'espace pour reprendre le slider
+    fireEvent.keyDown(window, { key: " ", code: "Space" });
+
+    // Vérifiez si le slider a repris la lecture
+    await screen.findByText("World Gaming Day");
+    await screen.findByText("World Farming Day");
   });
 });
